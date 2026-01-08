41d55e765f220e8d7f2f3dc6decc24820f71c4bf
VeruHorena <horenovska.veronika@gmail.com>
2026-01-08 10:33:32 +0100

    wpf

---
+++ Kalkulacka/Kalkulacka/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulacka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textVypis.Text = textVypis.Text + "1";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textVypis.Text = textVypis.Text + "2";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textVypis.Text = textVypis.Text + "3";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            string vyraz = textVypis.Text;
+            textVypis.Text = "";
+            if (string.IsNullOrWhiteSpace(vyraz))
+                textVypis.Text = "prazdny vyraz";
+            else
+            {
+                string[] cisla = new string[10];
+                cisla = vyraz.Split('+', 10);
+                for (int i = 0; i < cisla.Length; i++)
+                {
+                    textVypis.Text += cisla[i];
+                }
+                int cislo;
+                int soucet = 0;
+                for(int i = 0;i< cisla.Length; i++)
+                {
+                    if (int.TryParse(cisla[i], out cislo) == true)
+                        soucet = soucet + cislo;
+
+                }
+                textVypis.Text = "" + soucet;
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            textVypis.Text = textVypis.Text + "+";
+        }
+    }
+}
---
+++ Pozdrav2/Pozdrav2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pozdrav2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnPozdrav_Click(object sender, RoutedEventArgs e)
+        {
+            String uzivatel = jmeno.Text;
+            lblPozdrav.Content = "Ahoj uzivateli " + uzivatel;
+        }
+    }
+}
