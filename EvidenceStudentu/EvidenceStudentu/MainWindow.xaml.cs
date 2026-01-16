2b5e765a98fbf81ad589619e1af80d94d0d298fe
VeruHorena <horenovska.veronika@gmail.com>
2026-01-16 10:38:03 +0100

    wpfStudent

---
+++ EvidenceStudentu/EvidenceStudentu/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvidenceStudentu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Student> seznamStudentu = new List<Student>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string j, p;
+            int v;
+         j = txtjmeno.Text;
+            p = txtprijmeni.Text;
+           v = int.Parse(txtvek.Text);
+           Student s = new Student(j, p, v);
+            seznamStudentu.Add(s);
+            txbZobrazeni.Text = s.ToString();
+        }
+    }
+}
---
+++ EvidenceStudentu/EvidenceStudentu/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidenceStudentu
+{
+    internal class Student
+    {
+       static int pocet = 1;
+        int id;
+        string jmeno;
+        string prijmeni;
+        int vek;
+
+        public int Vek { get => vek; set => vek = value; }
+        public string Prijmeni { get => prijmeni; set => prijmeni = value; }
+        public string Jmeno { get => jmeno; set => jmeno = value; }
+        public int Id { get => id; set => id = value; }
+
+        public Student (string jm, string pr, int v)
+        {
+            if ((jm == "") || (pr == "") || (v == null))
+            {
+                id = pocet;
+                pocet++;
+                jmeno = "prazdne jmeno";
+                prijmeni = "prazdne prijmeni";
+                vek = 0;
+            }
+            else
+            {
+                id = pocet;
+                pocet++;
+                jmeno = jm;
+                prijmeni = pr;
+                vek = v;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Student id:" + this.id + "jmeno: " + this.jmeno + " prijmeni: " + this.prijmeni + " vek: " + this.vek;
+        }
+    }
+}
