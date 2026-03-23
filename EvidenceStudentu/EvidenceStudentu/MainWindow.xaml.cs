9b41e1cd039f1fc2bfde82624a986361af94ada7
VeruHorena <horenovska.veronika@gmail.com>
2026-03-23 10:35:04 +0100

    evidence

--- EvidenceStudentu/EvidenceStudentu/MainWindow.xaml.cs
+++ EvidenceStudentu/EvidenceStudentu/MainWindow.xaml.cs
@@ -31,58 +31,36 @@ namespace EvidenceStudentu
         {
             InitializeComponent();
             Database_Loaded();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string j, p;
-            int v;
-            j = txtjmeno.Text;
-            p = txtprijmeni.Text;
-            v = int.Parse(txtvek.Text);
-            Student s = new Student(j, p, v);
+            PridatStudenta okno = new PridatStudenta(this);
+            okno.ShowDialog();
 
-            seznamStudentu.Add(s);
-            var items = seznamStudentu;
-            lstbox.ItemsSource = null;
-            lstbox.ItemsSource = items;
-
-            //txbZobrazeni.Text = s.ToString();
         }
 
         private void lstboxChanged(object sender, SelectionChangedEventArgs e)
         {
             var selected = lstbox.SelectedItem as Student;
+            if (selected == null) return;
 
-            if (selected != null)
+            List<string> znamky = ZnamkyStudenta(selected.Id);
+
+            if (znamky.Count == 0)
             {
-                txtjmeno.Text = selected.Jmeno;
-                txtprijmeni.Text = selected.Prijmeni;
-                txtvek.Text = selected.Vek.ToString();
-
-                List<string> znamky = ZnamkyStudenta(selected.Id);
-
-                string vypis = string.Join(", ", znamky);
-
-                MessageBox.Show($"Známky studenta {selected.Jmeno} {selected.Prijmeni}: {vypis}");
+                MessageBox.Show("Student nemá žádné známky.");
             }
-
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (lstbox.SelectedItem is Student vybrany)
+            else
             {
-                vybrany.Jmeno = txtjmeno.Text;
-                vybrany.Prijmeni = txtprijmeni.Text;
-                vybrany.Vek = int.Parse(txtvek.Text);
-
-                // refresh ListBoxu
-                lstbox.ItemsSource = null;
-                lstbox.ItemsSource = seznamStudentu;
+                string text = string.Join(", ", znamky);
+                MessageBox.Show($"Známky studenta:\n{text}");
             }
         }
+        
+
+       
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
@@ -126,6 +104,7 @@ namespace EvidenceStudentu
         }
         public void StudentiDatabaze()
         {
+            seznamStudentu.Clear();
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
                 conn.Open();
@@ -148,7 +127,9 @@ namespace EvidenceStudentu
                         seznamStudentu.Add(s);
                     }  
                 }
-            }  
+            }
+            lstbox.ItemsSource = null;
+            lstbox.ItemsSource = seznamStudentu;
         }
         public void PridatStudenta(string jmeno, string prijmeni, int vek)
         {
@@ -194,6 +175,17 @@ namespace EvidenceStudentu
 
             return znamky;
         }
+        public void aktualizaceStranky()
+        {
+            lstbox.ItemsSource = null;
+            lstbox.ItemsSource = seznamStudentu;
+        }
+
+        public void pridejStudentaDoListu(Student s)
+        {
+            seznamStudentu.Add(s);
+        }
 
     }
+  
 }
---
+++ EvidenceStudentu/EvidenceStudentu/PridatStudenta.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EvidenceStudentu
+{
+    /// <summary>
+    /// Interakční logika pro PridatStudenta.xaml
+    /// </summary>
+    public partial class PridatStudenta : Window
+    {
+        private MainWindow hlavniOkno;
+        public PridatStudenta(MainWindow okno)
+        {
+        InitializeComponent();
+            hlavniOkno = okno;
+        }
+
+        private void BtnUlozit_Click(object sender, RoutedEventArgs e)
+        {
+            string jmeno = txtJmeno.Text;
+            string prijmeni = txtPrijmeni.Text;
+            int vek;           
+                vek = int.Parse(txtVek.Text);
+            
+            hlavniOkno.PridatStudenta(jmeno, prijmeni, vek);
+            Student s = new Student(jmeno, prijmeni, vek);
+            hlavniOkno.pridejStudentaDoListu(s);
+            hlavniOkno.aktualizaceStranky();
+            this.Close();
+        }
+    }
+}
